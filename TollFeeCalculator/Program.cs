9ca899a0ec76bd99d8179b85053834e960223b61
Majed Dalain <majed-dalain@hotmail.com>
2023-09-11 21:18:20 +0200

    Add project files.

---
+++ TollFeeCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TollFeeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
