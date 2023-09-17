d524fdad613be157f471225d3c0fcb38d48f77cf
Majed Dalain <majed-dalain@hotmail.com>
2023-09-17 20:54:45 +0200

    fix unit test

--- TollFeeCalculatorTest/TollFeeCalculator.Test.cs
+++ TollFeeCalculatorTest/TollFeeCalculator.Test.cs
@@ -1,41 +1,66 @@
+using Moq;
+using TollFeeCalculator.Services.FeeCalculators;
+using TollFeeCalculator.Services.PublicHolidayLoader;
+using TollFeeCalculator.Services.TollFreeConditions;
+
 namespace TollFeeCalculatorTest
 {
     [TestClass]
     public class TollCalculatorTests
     {
 
-        private static DefaultTollFreeChecker? tollFreeChecker;
+        private static ITollFreeChecker? _tollFreeCheckerForDaysOnly;
+        private static ITollFreeChecker? _tollFreeCheckerForVehicleAndDay;
 
         [ClassInitialize]
-        public static async Task Init(TestContext context) // TestContext parameter is required by MSTest for ClassInitialize
+        public static void Init(TestContext context) 
         {
-            var publicHolidayLoader = new PublicHolidayLoader();
-            var publicHolidays = await publicHolidayLoader.LoadPublicHolidaysAsync();
-            tollFreeChecker = new TollFreeChecker(publicHolidays);
+            var mockPublicHolidayLoader = new Mock<IPublicHolidayLoader>();
+            mockPublicHolidayLoader.Setup(loader => loader.LoadPublicHolidaysAsync())
+                         .ReturnsAsync(new List<DateTime> { new DateTime(2023, 1, 1), new DateTime(2023, 4, 2) });  // these are the holidays 
+
+             _tollFreeCheckerForDaysOnly = new DefaultTollFreeChecker(new List<ITollFreeCondition>
+                {
+                    new TollFreeDate(mockPublicHolidayLoader.Object.LoadPublicHolidaysAsync().Result,
+                                     new List<DayOfWeek> { DayOfWeek.Saturday, DayOfWeek.Sunday },
+                                     new List<int> { 7 })
+                });
+
+            _tollFreeCheckerForVehicleAndDay = new DefaultTollFreeChecker(new List<ITollFreeCondition>
+                {
+                    new TollFreeDate(mockPublicHolidayLoader.Object.LoadPublicHolidaysAsync().Result,
+                                     new List<DayOfWeek> { DayOfWeek.Saturday, DayOfWeek.Sunday },
+                                     new List<int> { 7 }),
+                    new TollFreeVehicle()
+                });
 
         }
 
         [TestMethod]
-        public void TestTollFeeForRegularCar()
+        public void TestTollFeeForRegularCarWithDailyChecker()
         {
-            var tollCalculator = new TollCalculator(tollFreeChecker);
+            Assert.IsNotNull(_tollFreeCheckerForDaysOnly, "_tollFreeCheckerForDaysOnly should not be null.");
+            var tollCalculator = new TollCalculator(_tollFreeCheckerForDaysOnly, new DefaultFeeCalculator());
             var dates = new List<DateTime>
-            {
-                new DateTime(2023, 09, 12, 08, 10, 0),
-                new DateTime(2023, 09, 12, 08, 15, 0),
-                new DateTime(2023, 09, 12, 08, 17, 0),
-            }.ToArray();
+                {
+                    new DateTime(2023, 09, 12, 08, 10, 0),
+                    new DateTime(2023, 09, 12, 08, 15, 0),
+                    new DateTime(2023, 09, 12, 08, 17, 0),
+                }.ToArray();
 
+        
             int totalFee = tollCalculator.GetTollFee(new Car(), dates);
 
-            Assert.AreEqual(13, totalFee); // based on your time table and logic 13 should be the fee
+            Assert.AreEqual(13, totalFee); // Based on your toll table and logic, 13 should be the fee
         }
 
+
         [TestMethod]
-        public void TestTollFeeForTollFreeVehicle()
+        public void TestTollFeeForTollFreeVehicleWithVehicleAndDayChecker()
         {
-                var tollCalculator = new TollCalculator(tollFreeChecker);
-                var dates = new List<DateTime>
+            Assert.IsNotNull(_tollFreeCheckerForVehicleAndDay, "_tollFreeCheckerForVehicleAndDay should not be null.");
+            var tollCalculator = new TollCalculator(_tollFreeCheckerForVehicleAndDay, new DefaultFeeCalculator());
+            var dates = new List<DateTime>
             {
                 new DateTime(2023, 09, 12, 08, 10, 0),
                 new DateTime(2023, 09, 12, 08, 15, 0),
@@ -48,42 +73,44 @@ namespace TollFeeCalculatorTest
         }
 
         [TestMethod]
-        public void TestTollFeeForWeekend()
+        public void TestTollFeeForFreeVehicleWithDailyChecker()
         {
-            var tollCalculator = new TollCalculator(tollFreeChecker);
+            Assert.IsNotNull(_tollFreeCheckerForDaysOnly, "_tollFreeCheckerForDaysOnly should not be null.");
+            var tollCalculator = new TollCalculator(_tollFreeCheckerForDaysOnly, new DefaultFeeCalculator());
             var dates = new List<DateTime>
             {
-                new DateTime(2023, 09, 10, 08, 10, 0), // Sunday
+                new DateTime(2023, 09, 12, 08, 10, 0), // Tuesday
             }.ToArray();
 
-            int totalFee = tollCalculator.GetTollFee(new Car(), dates);
+            int totalFee = tollCalculator.GetTollFee(new Motorbike(), dates);
 
-            Assert.AreEqual(0, totalFee); // Should be free on weekends
+            Assert.AreEqual(13, totalFee); // even thoug it is a motorbik, but here we use another calss that checks only based on days 
         }
 
         [TestMethod]
         public void TestTollFeeForDayBeforeHoliday()
         {
-                var tollCalculator = new TollCalculator(tollFreeChecker);
-                var dates = new List<DateTime>
+            Assert.IsNotNull(_tollFreeCheckerForDaysOnly, "_tollFreeCheckerForDaysOnly should not be null.");
+            var tollCalculator = new TollCalculator(_tollFreeCheckerForDaysOnly, new DefaultFeeCalculator());
+            var dates = new List<DateTime>
             {
-                new DateTime(2023, 06, 04, 08, 10, 0), // Thursday before Good friday in April
-                new DateTime(2023, 06, 04, 08, 30, 0), // Thursday before Good friday in April
+                new DateTime(2023, 04, 01, 08, 10, 0), // Thursday before Good friday in April
+                new DateTime(2023, 04, 01, 08, 30, 0), // Thursday before Good friday in April
             }.ToArray();
 
             int totalFee = tollCalculator.GetTollFee(new Car(), dates);
 
-            Assert.AreEqual(0, totalFee); // Should be free on weekends
+            Assert.AreEqual(0, totalFee); // Should be free on weekends and a day before holiday
         }
 
         [TestMethod]
         public void TestTollFeeForMultipleTripsDifferentHours()
         {
-                // Initialize tollCalculator with mocked public holidays or real API call
-                var tollCalculator = new TollCalculator(tollFreeChecker);
+            Assert.IsNotNull(_tollFreeCheckerForDaysOnly, "_tollFreeCheckerForDaysOnly should not be null.");
+            var tollCalculator = new TollCalculator(_tollFreeCheckerForDaysOnly, new DefaultFeeCalculator());
 
-                // Dates representing the times the car passed through the toll
-                var dates = new List<DateTime>
+            // Dates representing the times the car passed through the toll
+            var dates = new List<DateTime>
             {
                 new DateTime(2023, 09, 12, 06, 20, 0), // Fee should be 8
                 new DateTime(2023, 09, 12, 07, 40, 0), // Fee should be 18
@@ -96,12 +123,8 @@ namespace TollFeeCalculatorTest
 
             // Calculate the toll fee for these trips
             int totalFee = tollCalculator.GetTollFee(new Car(), dates);
-
-            // Validate the toll fee
-            // Should be min(8 + 18 + 8 + 13 + 18 + 13, 60) = min(78, 60) = 60
             Assert.AreEqual(52, totalFee);
         }
 
-        // Add more test methods to validate different time frames, public holidays, etc.
     }
 }
